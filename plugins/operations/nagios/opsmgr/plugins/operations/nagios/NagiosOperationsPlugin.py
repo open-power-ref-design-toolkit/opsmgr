1ab256efdaf63f0b940252e9fa098893e822fb5a
William Irons <wdirons@us.ibm.com>
2016-06-10 11:18:21 -0400

    220870 Call the nagios container from the horizon container to configure nagios

--- plugins/operations/nagios/opsmgr/plugins/operations/nagios/NagiosOperationsPlugin.py
+++ plugins/operations/nagios/opsmgr/plugins/operations/nagios/NagiosOperationsPlugin.py
@@ -1,22 +1,25 @@
 import os
 import re
-import subprocess
+
+import paramiko
+
+from backports import configparser
 from opsmgr.inventory.interfaces.IManagerDeviceHook import IManagerDeviceHook
 from opsmgr.inventory import persistent_mgr
 from opsmgr.common import exceptions
 
 class NagiosPlugin(IManagerDeviceHook):
 
-    NAGIOS_CONTAINER_NAME = "nagios"
-    CONFIG_PATH = "/etc/opsmgr/nagios_config"
-    COMMANDS_DIR = CONFIG_PATH + "/commands"
+    CONFIG_PATH = "/usr/local/nagios/opsmgr"
     HOSTS_DIR = CONFIG_PATH + "/hosts"
     CREDS_DIR = CONFIG_PATH + "/.creds"
     NAGIOS_CFG_MASTER_FILE = "/usr/local/nagios/etc/nagios.cfg"
-    LXC_COMMAND = "lxc-attach -n " + NAGIOS_CONTAINER_NAME + " -- "
-    VERIFY_COMMAND = LXC_COMMAND + "/usr/local/nagios/bin/nagios -v " + NAGIOS_CFG_MASTER_FILE
-    RELOAD_COMMAND = LXC_COMMAND + "service nagios reload"
-    LXC_COMMAND = "lxc-attach -n " + NAGIOS_CONTAINER_NAME
+    VERIFY_COMMAND = "/usr/local/nagios/bin/nagios -v " + NAGIOS_CFG_MASTER_FILE
+    RELOAD_COMMAND = "service nagios reload"
+
+    OPSMGR_CONF = "/etc/opsmgr/opsmgr.conf"
+    OPSMGR_SECTION = "NAGIOS"
+    OPSMGR_NAGIOS_SERVER = "server"
 
     @staticmethod
     def add_device_pre_save(device):
@@ -67,52 +70,107 @@ class NagiosPlugin(IManagerDeviceHook):
         """ Reload the nagios server from within the
             nagios container
         """
-        # Validate the nagios config is good
-        (rc, output) = subprocess.getstatusoutput(NagiosPlugin.VERIFY_COMMAND)
-        if rc != 0:
-            raise exceptions.OpsException("Validation of the nagios configuration failed:\n"
-                                          "Command: " + NagiosPlugin.VERIFY_COMMAND + "\n"
-                                          "Output: " + output + "\n")
+        nagios_host = NagiosPlugin._get_nagios_server()
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(nagios_host, timeout=30)
 
-        (rc, output) = subprocess.getstatusoutput(NagiosPlugin.RELOAD_COMMAND)
-        if rc != 0:
-            raise exceptions.OpsException("service reload of nagios failed:\n"
-                                          "Command: " + NagiosPlugin.RELOAD_COMMAND + "\n"
-                                          "Output: " + output + "\n")
+            # Validate the nagios config is good
+            (_stdin, stdout, _stderr) = client.exec_command(NagiosPlugin.VERIFY_COMMAND)
+            rc = stdout.channel.recv_exit_status()
+            output = stdout.read().decode()
+            if rc != 0:
+                raise exceptions.OpsException("Validation of the nagios configuration failed:\n"
+                                              "Command: " + NagiosPlugin.VERIFY_COMMAND + "\n"
+                                              "Output: " + output + "\n")
+            # Reload nagios service
+            (_stdin, stdout, _stderr) = client.exec_command(NagiosPlugin.RELOAD_COMMAND)
+            rc = stdout.channel.recv_exit_status()
+            output = stdout.read().decode()
+            if rc != 0:
+                raise exceptions.OpsException("service reload of nagios failed:\n"
+                                              "Command: " + NagiosPlugin.RELOAD_COMMAND + "\n"
+                                              "Output: " + output + "\n")
+        finally:
+            client.close()
+
     @staticmethod
     def _write_creds_file(address, userid, password, key_string):
-        if not os.path.exists(NagiosPlugin.CREDS_DIR):
-            os.mkdir(NagiosPlugin.CREDS_DIR)
-        target_file = NagiosPlugin.CREDS_DIR + "/." + address + ".creds"
-
-        if password is None:
-            password = ""
-        if key_string is None:
-            key_string = ""
-        with open(target_file, 'w') as target:
-            target.write(userid + "\n")
-            target.write(password + "\n")
-            target.write(key_string + "\n")
+        nagios_host = NagiosPlugin._get_nagios_server()
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(nagios_host, timeout=30)
+            sftp = client.open_sftp()
+            try:
+                sftp.mkdir(NagiosPlugin.CREDS_DIR)
+            except IOError:
+                #directory already exist
+                pass
+            file_handle = sftp.open(NagiosPlugin.CREDS_DIR + "/." + address + ".creds", 'w')
+            if password is None:
+                password = ""
+            if key_string is None:
+                key_string = ""
+            file_handle.write(userid + "\n")
+            file_handle.write(password + "\n")
+            file_handle.write(key_string + "\n")
+            file_handle.close()
+        finally:
+            client.close()
 
     @staticmethod
     def _write_config_file(label, device_type, address, hostname):
-        source_file = NagiosPlugin.HOSTS_DIR + "/" + device_type.replace(" ", "_") + ".host"
-        target_file = NagiosPlugin.HOSTS_DIR + "/" + address + ".cfg"
-        with open(source_file, 'r') as source:
+        nagios_host = NagiosPlugin._get_nagios_server()
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(nagios_host, timeout=30)
+            sftp = client.open_sftp()
+
+            source_file = NagiosPlugin.HOSTS_DIR + "/" + device_type.replace(" ", "_") + ".host"
+            target_file = NagiosPlugin.HOSTS_DIR + "/" + address + ".cfg"
+            source = sftp.open(source_file, 'r')
             lines = source.readlines()
-        with open(target_file, 'w') as target:
+            target = sftp.open(target_file, 'w')
             for line in lines:
                 line = re.sub(r'#@#IP#@#', address, line)
                 line = re.sub(r'#@#LABEL#@#', label, line)
                 line = re.sub(r'#@#HOSTNAME#@#', hostname, line)
                 target.write(line)
+        finally:
+            client.close()
 
     @staticmethod
     def _remove_config_files(address):
-        target_file = NagiosPlugin.HOSTS_DIR + "/" + address + ".cfg"
-        if os.path.exists(target_file):
-            os.remove(target_file)
-        target_file = NagiosPlugin.CREDS_DIR + "/" + address + ".creds"
-        if os.path.exists(target_file):
-            os.remove(target_file)
+        nagios_host = NagiosPlugin._get_nagios_server()
+        try:
+            client = paramiko.SSHClient()
+            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            client.connect(nagios_host, timeout=30)
+            sftp = client.open_sftp()
 
+            target_file = NagiosPlugin.HOSTS_DIR + "/" + address + ".cfg"
+            try:
+                sftp.remove(target_file)
+            except IOError:
+                #file already removed
+                pass
+            target_file = NagiosPlugin.CREDS_DIR + "/." + address + ".creds"
+            try:
+                sftp.remove(target_file)
+            except IOError:
+                #file already removed
+                pass
+        finally:
+            client.close()
+
+    @staticmethod
+    def _get_nagios_server():
+        server = None
+        if os.path.exists(NagiosPlugin.OPSMGR_CONF):
+            parser = configparser.ConfigParser()
+            parser.read(NagiosPlugin.OPSMGR_CONF, encoding='utf-8')
+            server = parser.get(NagiosPlugin.OPSMGR_SECTION, NagiosPlugin.OPSMGR_NAGIOS_SERVER)
+        return server
