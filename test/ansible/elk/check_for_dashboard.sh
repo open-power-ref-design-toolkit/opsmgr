a688615bdfe47620b35a84960d59f65c44a7f47f
Mary A Mitchell <mamitchl@us.ibm.com>
2016-06-24 09:16:39 -0400

    221133 Check that Log Analyze and Log Management dashboards are installed in kibana

--- test/ansible/elk/check_for_dashboard.sh
+++ test/ansible/elk/check_for_dashboard.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
+# Copyright 2016, IBM US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This script checks that a particular dashboard is installed in Kibana on 
 # a particular ELK host.
 #
-# DASHBOARD and  ELK_IP must be passed from the command line
-#
+# DASHBOARD, ELK_IP, and ELASTIC_PORT must be passed from the command line 
+# 
 # Example to invoke:
-# check_for_dashboard.sh -i 9.27.24.69 -d "Log Analyze Dashboard - Logs-From GUI" 
+# check_for_dashboard.sh -i 9.27.24.69 -e 9200 -d "Log Analyze Dashboard - Logs-From GUI" 
 #
 
 # Get the command line options that were passed to the command
 source test_options.sh
 
 # Make sure all required options were passed
-if [ -z '${DASHBOARD}' ] || [ -z ${ELK_IP} ];
+if [ -z '${DASHBOARD}' ] || [ -z ${ELK_IP} ] || [ -z ${ELASTIC_PORT} ];
 then
-   echo "Dashboard and elk ip are required inputs for check_for_dashboard"
+   echo 
+   echo "Dashboard, ELK IP, and Elasticsearch port are required inputs for check_for_dashboard"
+   echo 
    echo "${SYNTAX}"
    exit 1
 fi
 echo "DASHBOARD:   ${DASHBOARD}"
 echo "ELK_IP:      ${ELK_IP}"
+echo "ELASTIC_PORT:    ${ELASTIC_PORT}" 
 
 # Check for the dashboard
-url="http://${ELK_IP}:9200/.kibana/_search?pretty&q=_type:dashboard"
-result=`curl $url --silent | grep "${DASHBOARD}"`
+url="http://${ELK_IP}:${ELASTIC_PORT}/.kibana/_search?pretty&q=_type:dashboard"
+result=`curl $url --silent | grep "\"${DASHBOARD}\""`
 
-if [ -z '${result}' ];
+if [ -z "${result}" ];
 then
   echo "Dashboard ${DASHBOARD} not installed"
   exit 1
--- test/ansible/elk/test_options.sh
+++ test/ansible/elk/test_options.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Copyright 2016, IBM US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This script processes the common test case command line options
 # Source this file in your script to read command line options and set variables
 
 OPTIONS=$*
+/bin/echo
 /bin/echo "Options passed in are ${OPTIONS}"
 
 SYNTAX=$(/bin/cat << EOT
@@ -12,21 +27,15 @@ Syntax:
  -d dashboard (enclose in quotes if it contains spaces)
  -i IP address for elk
  -e port for elasticsearch
- -k port for kibana
- -u userid for kibana
- -p password for kibana
 EOT
 )
-while getopts "d:i:e:k:u:p:x" FLAG;
+while getopts "d:i:e:x" FLAG;
 do 
   case ${FLAG} in
     x) set -x ;;
     d) export DASHBOARD=${OPTARG} ;;
     i) export ELK_IP=${OPTARG} ;;
     e) export ELASTIC_PORT=${OPTARG} ;;
-    k) export KIBANA_PORT=${OPTARG} ;;
-    u) export KIBANA_USER=${OPTARG} ;;
-    p) export KIBANA_PW=${OPTARG} ;;
     *) echo "${SYNTAX}" && exit 1 ;;
   esac
 done
