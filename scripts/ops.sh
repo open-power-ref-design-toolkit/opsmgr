5fcd2d3bc9b3a5db03af9b2070608e1b69b87e47
William irons <wdirons@us.ibm.com>
2016-09-06 15:06:06 -0400

    Workitem 221614 Fixes for opsmgr playbooks
    Fixed several issues here:
    Added a separate step in scripts/ops.sh to create the lxc containers.
      This is so the site.yml playbooks had no issue finding the containers.
    Removed iptables for standalone, using haproxy for both standalone
      and OSA
    Fixed the issue with the hostname being incorrect for the all in one
      install.
    
    Cleaned up variable file confusion:
    ../ext/overrides.yml and ../ext/shared.yml is for the OSA install
    defaults/overrides.yml and defaults/shared.yml is for the standalone
      install.
    Because shared.yml contains host being added dynamically, I had to
      load it using the include_vars rule.
    
    Fixed a couple of nagios bugs:
     -- nagios plugins run with python3 and require paramiko
     -- downleveled paramiko because of a bug in paramiko
     -- fixed creds filename to use address instead of hostname, as that
          is what the plugin is expecting
    
    Change-Id: I93b8e591333d12487da2a7a1e984cf24ca0e2353

--- scripts/ops.sh
+++ scripts/ops.sh
@@ -10,11 +10,13 @@ scripts/clean.sh
 
 # bootstraps ssh proxy, keys & sudoers
 # note: requires .ssh/id_rsa|id_rsa.pub
+echo "from playbooks running local.yml"
 pushd playbooks
 ansible-playbook -i inventory local.yml
 popd
 
 # predeploy integrates opsmgr with osa
+echo "from predeploy/osa running local.yml"
 pushd predeploy/osa
 ansible-playbook -i inventory local.yml
 popd
@@ -22,7 +24,14 @@ popd
 # stages generated inventory file
 cp -f ext/inventory playbooks
 
+# creates the containers
+echo "from playbooks running hosts.yml"
+pushd playbooks
+ansible-playbook -i inventory hosts.yml
+popd
+
 # deploys opsmgr and integration plugins
+echo "from playbooks running site.yml"
 pushd playbooks
 ansible-playbook -i inventory site.yml
 popd
