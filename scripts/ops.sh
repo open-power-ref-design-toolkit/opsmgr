b8d235945ab585ddfe337bc729e7fc6777404fbe
William irons <wdirons@us.ibm.com>
2016-09-12 10:02:11 -0400

    Adding return codes in install scripts
    Added unique return codes for failures in each call of an ansible
    playbook from ops.sh. Added unique return codes for failures calling
    the elk and ops.sh scripts from within create-cluster-opsmgr.sh
    
    This is so higher level scripts to catch the failures.
    
    Change-Id: Ie9bd11343463176327e2a8ee1eea68306eff5641

--- scripts/ops.sh
+++ scripts/ops.sh
@@ -13,12 +13,22 @@ scripts/clean.sh
 echo "from playbooks running local.yml"
 pushd playbooks
 ansible-playbook -i inventory local.yml
+rc=$?
+if [ $rc != 0 ]; then
+    echo "Failed running opsmgr playbooks/local.yml, rc=$rc"
+    exit 2
+fi
 popd
 
 # predeploy integrates opsmgr with osa
 echo "from predeploy/osa running local.yml"
 pushd predeploy/osa
 ansible-playbook -i inventory local.yml
+rc=$?
+if [ $rc != 0 ]; then
+    echo "Failed running opsmgr predeploy/osa/local.yml, rc=$rc"
+    exit 3
+fi
 popd
 
 # stages generated inventory file
@@ -28,12 +38,22 @@ cp -f ext/inventory playbooks
 echo "from playbooks running hosts.yml"
 pushd playbooks
 ansible-playbook -i inventory hosts.yml
+rc=$?
+if [ $rc != 0 ]; then
+    echo "Failed running opsmgr playbooks/hosts.yml, rc=$rc"
+    exit 4
+fi
 popd
 
 # deploys opsmgr and integration plugins
 echo "from playbooks running site.yml"
 pushd playbooks
 ansible-playbook -i inventory site.yml
+rc=$?
+if [ $rc != 0 ]; then
+    echo "Failed running opsmgr playbooks/site.yml, rc=$rc"
+    exit 5
+fi
 popd
 
 popd
