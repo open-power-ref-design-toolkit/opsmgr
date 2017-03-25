a8c6aad12bd23ff56632c9f4b95b4bfe629b3dcf
William Irons <wdirons@us.ibm.com>
2017-03-25 16:23:16 -0400

    Redhat Server with no containers
    Support for installing on a Redhat Server without using lxc containers
    instead everything is installed directly on the localhost
    
    Change-Id: I87070c33a70c909cc7d22a5c35851f1f2ac25e4d

--- plugins/provisioning/base/nagios/commands/server.cmd
+++ plugins/provisioning/base/nagios/commands/server.cmd
@@ -2,9 +2,9 @@
 
 command[server-cpu]      = sudo /etc/nagios/plugins/check-cpu.rb -w 80 -c 90
 command[server-mem]      = sudo /etc/nagios/plugins/check-mem.sh -w 250 -c 100
-command[server-disk]     = sudo /etc/nagios/plugins/check-disk.rb 
+command[server-disk]     = sudo /etc/nagios/plugins/check-disk.rb -x iso9660
 command[server-logs]     = sudo /etc/nagios/plugins/check-for-large-files.sh -d /var/log -s 1048576
-command[server-socket]   = sudo /etc/nagios/plugins/check-syslog-socket.rb 
+command[server-socket]   = sudo /etc/nagios/plugins/check-syslog-socket.rb
 command[server-rsyslogd] = sudo /etc/nagios/plugins/check-procs.rb -p rsyslogd -w 80 -c 320 -W 1 -C 1
 command[server-sshd]     = sudo /etc/nagios/plugins/check-procs.rb -p sshd -w 80 -c 320 -W 1 -C 1
 command[server-cron]     = sudo /etc/nagios/plugins/check-procs.rb -p cron -w 80 -c 320 -W 1 -C 1
